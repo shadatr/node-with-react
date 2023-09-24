31bdf2ef5188c9de9e9a36390ba45eff3c9bf2f6
shadatr <shadadaab@gmail.com>
2023-09-24 18:06:16 +0300

    authenticatin added

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-const express= require('express')
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy; // Correct import
 
-const app= express()
+const keys = require("./config/keys");
+
+const app = express();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID, // Use keys.googleClientID
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+const PORT = 5000;
+app.listen(PORT);
