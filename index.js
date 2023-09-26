bea8c8fd396d1475d7f575c9259978d73ee8dadc
shadatr <shadadaab@gmail.com>
2023-09-26 21:46:46 +0300

    authentications added

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy; // Correct import
-
 const keys = require("./config/keys");
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+require("./modules/User");
+require("./sevices/passport");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID, // Use keys.googleClientID
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken) => {
-      console.log(accessToken);
-    }
-  )
-);
+app.use(cookieSession({
+  maxAge: 30*24*60*60*1000,
+  keys: [keys.cookieKey]
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+require("./routes/authRoutes")(app)
 
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = 5000;
 app.listen(PORT);
---
+++ modules/User.js
@@ -0,0 +1,6 @@
+const mongoose=require('mongoose')
+const {Schema}= mongoose;
+
+const userSchema=new Schema({googleId:String});
+
+mongoose.model('users',userSchema)
