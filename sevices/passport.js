d6ef0e5e0ecebc199ba47dfdc72eafc13fd96cfb
shadatr <shadadaab@gmail.com>
2023-09-28 17:06:31 +0300

    proxy:true

--- sevices/passport.js
+++ sevices/passport.js
@@ -22,6 +22,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
+      proxy:true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then((existingUser) => {
