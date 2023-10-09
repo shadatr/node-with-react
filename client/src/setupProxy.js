d5b5531e70eb8edb03744330b15c56d0123f6ca3
shadatr <shadadaab@gmail.com>
2023-10-09 16:49:54 +0300

    setupproxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,9 +1,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
-  app.use(
-    ['/api', '/auth/google'],
-    createProxyMiddleware({
-      target: 'http://localhost:5000',
-    })
-  );
+  try{
+    app.use(
+      ['/api', '/auth/google'],
+      createProxyMiddleware({
+        target: 'http://localhost:5000',
+      })
+    );
+  }
+  catch(err){
+    console.log(err);
+ }
 };
--- index.js
+++ index.js
@@ -35,5 +35,5 @@ if( process.env.NODE_ENV==='production'){
   })
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT);
