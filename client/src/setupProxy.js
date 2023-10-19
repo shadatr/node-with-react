456737c715a8f01bf955ba2d9dbacf501fe99ff3
shadatr <shadadaab@gmail.com>
2023-10-19 20:57:22 +0300

    domain edited

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
       ['/api', '/auth/google'],
       createProxyMiddleware({
-        target: 'http://localhost:5000',
+        target: process.env.DOMIAN,
       })
     );
   }
