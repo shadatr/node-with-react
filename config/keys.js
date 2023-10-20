8e2fd618e7d80b399455ee347b260f3b2882807d
shadatr <shadadaab@gmail.com>
2023-10-20 09:32:24 +0300

    keys

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,12 @@
-// keys.js - figure out what set of credentials to return
-if (process.env.NODE_ENV === 'production') {
-  // we are in production - return the prod set of keys
-  module.exports = require('./prod');
-} else {
-  // we are in development - return the dev keys!!!
-  module.exports = require('./dev');
-}
+module.exports = {
+  googleClientID:
+    "253929080006-046gatrtlms1c7rbbto9q6upcm9g7bvq.apps.googleusercontent.com",
+  googleClientSecret: "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",
+  mongoURI: 'mongodb+srv://shadadaab:m6lHdeaBb2zL8tqa@cluster0.ajebrhw.mongodb.net/?retryWrites=true&w=majority',
+  cookieKey: 'redacted',
+  stripePublishableKey: "pk_test_51NxWTyLqIYH2rTG4HaynpaGoRam0ArPn2sY480u5k0vuktweaawwD8pSDLfyjpl7zcjdanOMWFH124BSaqo0xTCH00APiQ53rI",
+  stripeScretKey: "sk_test_51NxWTyLqIYH2rTG4FDSJBx2Jls4npDi9qqxWS2YwITEa8231bbRCvL26WGNnEivi3bjTIUQZc4p2qdIBqGBXcbmf00pteQsjY8",
+  gmailUser: "emsender@gmail.com",
+  gmailPass: "wxyj klif zykd iiev",
+  redirectDomain: "http://localhost:3000"
+};
